89b668fff5e5f6234908ff503ff5364db75bdb89
AlvinBach <quocanh1990bach@gmail.com>
2021-02-09 21:28:22 +0200

    making cartItem component and connect cart dropdown to the cartItems reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addItem } from "./../../redux/cart/cart.action";
 import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 
-const CollectionItem = ({ id, name, imageUrl, price }) => {
-  console.log(imageUrl);
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
     <div className="collection-item">
       <div
@@ -13,8 +16,15 @@ const CollectionItem = ({ id, name, imageUrl, price }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted="true">
+        Add To Cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/pages/hatspage/hatspage.component.jsx
+++ src/pages/hatspage/hatspage.component.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import data from "../../pages/shoppage/shop.data";
+const { items } = data[0];
 
 const HatsPage = () => {
-  return <div>Hats Page</div>;
+  return (
+    <div>
+      {items.map(({ id, ...otherProps }) => (
+        <CollectionItem key={id} {...otherProps} />
+      ))}
+    </div>
+  );
 };
 
 export default HatsPage;
