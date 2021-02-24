304e1b9571c4780073b596abf2ac7e97ff7d025a
AlvinBach <quocanh1990bach@gmail.com>
2021-02-24 17:42:27 +0200

    updating stripe payment

--- src/pages/checkoutpage/checkout.component.jsx
+++ src/pages/checkoutpage/checkout.component.jsx
@@ -7,6 +7,7 @@ import {
   selectCartTotal,
 } from "../../redux/cart/cart.selectors";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import "./checkout.styles.scss";
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -32,6 +33,12 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">TOTAL: ${total}</div>
+    <div className="text-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
